9c52feac29135a103df5aa74baf54c170b27b55c
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 00:05:25 +0800

    feat(page): new store lists page

---
+++ pages/lists/[id].vue
@@ -0,0 +1,47 @@
+<template>
+  <div v-for="(info, index) in infos" :key="index">
+    <list-bar :item="info" @delItem="delItem"></list-bar>
+  </div>
+</template>
+
+<script lang="tsx" setup>
+import { IListItem } from '@/composables/IListItem'
+</script>
+
+<script lang="tsx">
+
+class StoreInfo implements IListItem {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+function mockInfo(): Array<IListItem> {
+  return [
+    new StoreInfo("Cafe"),
+    new StoreInfo("7-11")
+  ]
+}
+
+export default {
+  methods: {
+    delItem(name) {
+      console.log('emit', name)
+    }
+  },
+  computed: {
+    infos() {
+      const userId: string = this.$route.params.id
+
+      console.log('user', userId)
+      return mockInfo()
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
