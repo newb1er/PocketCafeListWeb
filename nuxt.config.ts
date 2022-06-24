ccd1bb19cba261e4cd2ed12cf0c0cdb883c901e1
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 00:03:49 +0800

    chore(add dep): element-plus

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,21 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    meta: {
+        title: "ElementPlus",
+        meta: [
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            {
+                hid: "description",
+                name: "description",
+                content: "ElementPlus + Nuxt3",
+            },
+        ]
+    },
+
+    css: [
+        "~/assets/scss/index.scss"
+    ],
+
+    components: true
 })
