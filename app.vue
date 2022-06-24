e4d66366ba87eca2f03dcc934b504a499c4e7361
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 00:03:31 +0800

    feat: add layout

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <h1>Pocket Cafe List</h1>
+      </el-header>
+      <el-main>
+        <NuxtPage />
+      </el-main>
+    </el-container>
   </div>
 </template>
+
+<script setup>
+import { ElContainer, ElHeader, ElMain } from "element-plus";
+</script>
