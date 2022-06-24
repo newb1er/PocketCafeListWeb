f27c6470186a273c018ae88402b1334d143622f2
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 02:32:27 +0800

    feat: reactive header title

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <div class="common-layout">
     <el-container>
       <el-header>
-        <h1>Pocket Cafe List</h1>
+        <h1>{{ header }}</h1>
       </el-header>
       <el-main>
         <NuxtPage />
@@ -13,4 +13,7 @@
 
 <script setup>
 import { ElContainer, ElHeader, ElMain } from "element-plus";
+import { useHeader } from "./composables/useHeader";
+
+const header = useHeader()
 </script>
