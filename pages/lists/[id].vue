01a70e8d7acbe48b7785c363bd436c56f6aec074
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 01:30:09 +0800

    feat: load saved list from api

--- pages/lists/[id].vue
+++ pages/lists/[id].vue
@@ -1,14 +1,19 @@
 <template>
-  <div v-for="(info, index) in infos" :key="index">
-    <list-bar :item="info" @delItem="delItem"></list-bar>
+  <div v-if="!loading">
+    <div v-for="(info, index) in infos" :key="index">
+      <list-bar :item="info" @delItem="delItem"></list-bar>
+    </div>
   </div>
 </template>
 
 <script lang="tsx" setup>
 import { IListItem } from '@/composables/IListItem'
+
+const loading = ref(false)
 </script>
 
 <script lang="tsx">
+import { getSavedList } from '@/api/saved'
 
 class StoreInfo implements IListItem {
   name: string
@@ -18,27 +23,29 @@ class StoreInfo implements IListItem {
   }
 }
 
-function mockInfo(): Array<IListItem> {
-  return [
-    new StoreInfo("Cafe"),
-    new StoreInfo("7-11")
-  ]
-}
-
 export default {
+  data() {
+    return {
+      userId: "",
+      infos: []
+    }
+  },
+  async mounted() {
+    this.loading = true
+    this.userId = this.$route.params.id
+
+    const res = await getSavedList(this.userId)
+
+    console.log(res)
+
+    this.infos = res.data
+    this.loading = false
+  },
   methods: {
     delItem(name) {
       console.log('emit', name)
     }
   },
-  computed: {
-    infos() {
-      const userId: string = this.$route.params.id
-
-      console.log('user', userId)
-      return mockInfo()
-    }
-  }
 }
 </script>
 
