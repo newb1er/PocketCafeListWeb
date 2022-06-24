0e2a2d079fca1a7b17a181f84dd70f47e2af8837
newb1er <32424677+newb1er@users.noreply.github.com>
2022-06-25 02:32:38 +0800

    feat: show saved info

--- pages/lists/[id].vue
+++ pages/lists/[id].vue
@@ -1,12 +1,75 @@
 <template>
   <div v-if="!loading">
-    <div v-for="(info, index) in infos" :key="index">
-      <list-bar :item="info" @delItem="delItem"></list-bar>
-    </div>
+    <el-collapse>
+      <el-collapse-item v-for="(info, index) in infos" :title="info.name" :key="index">
+        <el-row class="flex">
+          <span>
+            {{ cityMap(info.city) }}
+          </span>
+        </el-row>
+        <el-row>
+          <span>新增時間:</span>
+          {{ dateParse(info.add_date) }}
+        </el-row>
+        <el-row>
+          <span class="text">網站: {{ info.url }}</span>
+        </el-row>
+        <el-row>地址: {{ info.address }}</el-row>
+        <el-row>
+          <span class="text">WiFi: </span>
+          <el-rate
+            v-model="info.wifi"
+            disabled
+            show-score
+          />
+        </el-row>
+        <el-row>
+          <span class="text">Seat: </span>
+          <el-rate
+            v-model="info.seat"
+            disabled
+            show-score
+          />
+        </el-row>
+        <el-row>
+          <span class="text">Quiet: </span>
+          <el-rate
+            v-model="info.quiet"
+            disabled
+            show-score
+          />
+        </el-row>
+        <el-row>
+          <span class="text">Tasty: </span>
+          <el-rate
+            v-model="info.tasty"
+            disabled
+            show-score
+          />
+        </el-row>
+        <el-row>
+          <span class="text">Cheap: </span>
+          <el-rate
+            v-model="info.cheap"
+            disabled
+            show-score
+          />
+        </el-row>
+        <el-row>
+          <span class="text">Music: </span>
+          <el-rate
+            v-model="info.music"
+            disabled
+            show-score
+          />
+        </el-row>
+      </el-collapse-item>
+    </el-collapse>
   </div>
 </template>
 
 <script lang="tsx" setup>
+import { ElCollapse, ElCollapseItem, ElRow, ElRate } from 'element-plus'
 import { IListItem } from '@/composables/IListItem'
 
 const loading = ref(false)
@@ -14,6 +77,7 @@ const loading = ref(false)
 
 <script lang="tsx">
 import { getSavedList } from '@/api/saved'
+import { cities } from '~~/utils/cityMapping'
 
 class StoreInfo implements IListItem {
   name: string
@@ -44,11 +108,25 @@ export default {
   methods: {
     delItem(name) {
       console.log('emit', name)
+    },
+    cityMap(enName) {
+      return cities[enName]
+    },
+    dateParse(str: string) {
+      const date = new Date(str)
+      return date.toLocaleDateString()
     }
   },
 }
 </script>
 
 <style>
+.text {
+  margin: auto 0;
+}
 
+.flex {
+  display: "flex";
+  flex: 1;
+}
 </style>
